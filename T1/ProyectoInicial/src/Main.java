223a1b49c55fb4ba1deb3e947d20f4bf0a9610d4
aseralcala <aser.alcala@gmail.com>
2025-10-19 17:54:05 +0200

    update

--- T1/ProyectoInicial/src/Main.java
+++ T1/ProyectoInicial/src/Main.java
@@ -1,18 +1,17 @@
 public class Main {
-public static void main(String [] args){
-    // ejemplos con Salto de línea
-    System.out.println("Primera fila con Salto de línea");
-    System.out.println("Segunda fila con Salto de línea");
-    System.out.println("Tercera fila con Salto de línea");
-    System.out.println("Cuarta fila con Salto de línea");
-    // ejemplos sin Salto de línea
-    System.out.print("Primera fila sin Salto de línea");
-    System.out.print("Segunda fila sin Salto de línea");
-    System.out.print("Tercera fila sin Salto de línea");
-    System.out.print("Cuarta fila sin Salto de línea");
-    System.out.println();
-    // ejemplos de hacer un salto de linea sin println
-    System.out.print("Primera fila sin Salto de línea pero con salto de linea n \n Segunda fila sin Salto de línea pero con salto de linea n \n");
-}
-
+    public static void main(String [] args) {
+        // ejemplos con Salto de línea
+        System.out.println("Primera fila con Salto de línea");
+        System.out.println("Segunda fila con Salto de línea");
+        System.out.println("Tercera fila con Salto de línea");
+        System.out.println("Cuarta fila con Salto de línea");
+        // ejemplos sin Salto de línea
+        System.out.print("Primera fila sin Salto de línea");
+        System.out.print("Segunda fila sin Salto de línea");
+        System.out.print("Tercera fila sin Salto de línea");
+        System.out.print("Cuarta fila sin Salto de línea");
+        System.out.println();
+        // ejemplos de hacer un salto de linea sin println
+        System.out.print("Primera fila sin Salto de línea pero con salto de linea manual n\nSegunda fila sin Salto de línea pero con salto de linea manual n\n");
+    }
 }
